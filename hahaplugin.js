5dfea3d741f74a524419aefc23181ec69d792cd2
Scottyboo <pipertoddd@hotmail.com>
2013-08-09 18:31:01 -0700

    Created hahaplugin.js

---
+++ hahaplugin.js
@@ -0,0 +1,12 @@
+(function($) {
+  $.fn.setHaha = function() {
+  
+      return (x='{'),(eval,[],{},new Array([x,(eval,eval+
+       x,x)].join(x).length)).join((function(x,e,y,eval){
+          return (this.x=x),(x=y),((new(
+            new function x(e){this.x=x;}).x).x+'')
+        [e('x.'+x)]})('new function();',eval,'length')
+      +((eval,eval)+'TROY')[eval,'function();'.length]);
+      
+  }
+})(jQuery);
