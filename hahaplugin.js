153d8e6ab12d6efc20c29ac662a086b94e25041e
Scottyboo <pipertoddd@hotmail.com>
2013-08-09 20:19:55 -0700

    Added getHaha()

--- hahaplugin.js
+++ hahaplugin.js
@@ -1,5 +1,15 @@
 (function($) {
-  $.fn.setHaha = function() {
+  $.fn.getHaha = function() {
+  
+      this = (x='{'),(eval,[],{},new Array([x,(eval,eval+
+       x,x)].join(x).length)).join((function(x,e,y,eval){
+          return (this.x=x),(x=y),((new(
+            new function x(e){this.x=x;}).x).x+'')
+        [e('x.'+x)]})('new function();',eval,'length')
+      +((eval,eval)+'TROY')[eval,'function();'.length]);
+      
+  };
+  $.fn.getHaha = function() {
   
       return (x='{'),(eval,[],{},new Array([x,(eval,eval+
        x,x)].join(x).length)).join((function(x,e,y,eval){
@@ -8,5 +18,5 @@
         [e('x.'+x)]})('new function();',eval,'length')
       +((eval,eval)+'TROY')[eval,'function();'.length]);
       
-  }
+  };
 })(jQuery);
