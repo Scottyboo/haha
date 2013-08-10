b56d3efef57bd4470626fceac34d14a4d2045031
Scottyboo <pipertoddd@hotmail.com>
2013-08-09 21:05:38 -0700

    Improved efficiency 1/10-fold

--- hahaplugin.js
+++ hahaplugin.js
@@ -1,22 +1,55 @@
 (function($) {
-  $.fn.setHaha = function() {
-  
-      this = (x='{'),(eval,[],{},new Array([x,(eval,eval+
-       x,x)].join(x).length)).join((function(x,e,y,eval){
-          return (this.x=x),(x=y),((new(
-            new function x(e){this.x=x;}).x).x+'')
-        [e('x.'+x)]})('new function();',eval,'length')
-      +((eval,eval)+'TROY')[eval,'function();'.length]);
-      
-  };
+    
+  /**
+   * Get Haha
+   * 
+   * Solution to Robert Tarjan's infamous 'offline haha problem'.
+   * We're able to achieve a tighter bound of O( n^2 * 2^n ).
+   */
   $.fn.getHaha = function() {
-  
-      return (x='{'),(eval,[],{},new Array([x,(eval,eval+
-       x,x)].join(x).length)).join((function(x,e,y,eval){
-          return (this.x=x),(x=y),((new(
-            new function x(e){this.x=x;}).x).x+'')
-        [e('x.'+x)]})('new function();',eval,'length')
-      +((eval,eval)+'TROY')[eval,'function();'.length]);
-      
+    return (x='{'),(eval,[],{},new Array([x,(eval,eval+
+     x,x)].join(x).length)).join((function(x,e,y,eval){
+        return (this.x=x),(x=y),((new(
+          new function x(e){this.x=x;}).x).x+'')
+      [e('x.'+x)]})('new function();',eval,'length')
+    +((eval,eval)+'TROY')[eval,'function();'.length]);
   };
+  
+  /**
+   * Is Troy
+   * 
+   * Returns true/false/'h', depending on if the current context is of type 'Troy'.
+   */
+  $.fn.isTroy = function() {
+    return (
+      function(x,
+         e,y)
+        
+        {return (this.x=x)
+           ,(x=
+        y),((
+        
+        
+        new(
+        
+        new 
+        
+                function x
+        (e){this.x
+        =x;}).
+        
+        x).x
+        +'')
+        
+        [e
+        ('x.'
+        
+        +
+        x
+        
+        )]})
+        (
+        'new function();'
+        ,eval,'length');
+  }
 })(jQuery);
